fb380dfef7be7ef05a2b56b053c53b029db1fcc1
mikecolbert <michael-colbert@uiowa.edu>
2023-11-10 17:26:03 -0600

    added logging and diagnostics

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, render_template, redirect, url_for
 import pymysql
 import os
+import logging
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 # 11/10/2023 MWC
 
+# create logger
+logging.basicConfig(
+    level=logging.INFO,
+    filename="log_file.log",
+    filemode="a",  # append to the log file
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logging.info("Loading variables from Azure Key Vault")
+
 AZURE_KEY_VAULT_URL = os.environ["AZURE_KEY_VAULT_URL"]
+
+logging.info(AZURE_KEY_VAULT_URL)
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=AZURE_KEY_VAULT_URL, credential=credential)
@@ -17,6 +30,13 @@ _dbpassword = client.get_secret("HW13-DBPASSWORD")
 _dbname = client.get_secret("HW13-DBNAME")
 _secret = client.get_secret("HW13-SECRET-KEY")
 
+logging.info("_dbhostname: " + _dbhostname.value)
+logging.info("_dbusername: " + _dbusername.value)
+logging.info("_dbpassword: " + _dbpassword.value)
+logging.info("_dbname: " + _dbname.value)
+logging.info("_secret: " + _secret.value)
+
+
 conn = pymysql.connect(
     host=_dbhostname.value,
     user=_dbusername.value,
@@ -25,6 +45,8 @@ conn = pymysql.connect(
     ssl={"ca": "./DigiCertGlobalRootCA.crt.pem"},
     cursorclass=pymysql.cursors.DictCursor,
 )
+
+logging.info("Starting Flask app")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = _secret.value
@@ -71,6 +93,59 @@ def search():
             )
     else:
         return redirect(url_for("index"))
+
+
+@app.route("/diagnostics", methods=["GET"])
+def diagnostics():
+    def diagnostics():
+        # borrowed from https://github.com/balarsen/FlaskStatus
+        platform_stats = {
+            "architecture": platform.architecture(),
+            "machine": platform.machine(),
+            "node": platform.node(),
+            "platform": platform.platform(),
+            "processor": platform.processor(),
+            "python_branch": platform.python_branch(),
+            "python_build": platform.python_build(),
+            "python_compiler": platform.python_compiler(),
+            "python_implementation": platform.python_implementation(),
+            "python_revision": platform.python_revision(),
+            "python_version": platform.python_version(),
+            "python_version_tuple": platform.python_version_tuple(),
+            "release": platform.release(),
+            "system": platform.system(),
+            "uname": platform.uname(),
+            "version": platform.version(),
+            "java_ver": platform.java_ver(),
+            "win32_ver": platform.win32_ver(),
+            "mac_ver": platform.mac_ver(),
+            "libc_ver": platform.libc_ver(),
+            "load_average": os.getloadavg(),
+        }
+        log_stats = []
+        log_files = [
+            "/tmp/gunicorn-pwnedapi.log",
+            "/tmp/gunicorn-pwnedhub.log",
+            "/tmp/gunicorn-pwnedspa.log",
+            "/tmp/gunicorn-pwnedsso.log",
+            "/var/log/nginx/access.log",
+        ]
+        for log_file in log_files:
+            if os.path.exists(log_file):
+                data = {
+                    "name": log_file,
+                    "size": os.path.getsize(log_file),
+                    "mtime": os.path.getmtime(log_file),
+                    "ctime": os.path.getctime(log_file),
+                    "tail": [],
+                }
+                with open(log_file) as fp:
+                    data["tail"] = "".join(fp.readlines()[-20:])
+                log_stats.append(data)
+
+        return render_template(
+            "diagnostics.html", platform_stats=platform_stats, log_stats=log_stats
+        )
 
 
 if __name__ == "__main__":
